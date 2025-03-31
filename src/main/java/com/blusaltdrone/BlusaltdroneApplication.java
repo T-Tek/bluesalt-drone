06d2dac48720a07eec6e5d7b750342b9a997b370
T-Tek <toppytech@gmail.com>
2025-03-31 10:38:02 +0100

    initial commit

---
+++ src/main/java/com/blusaltdrone/BlusaltdroneApplication.java
@@ -0,0 +1,13 @@
+package com.blusaltdrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlusaltdroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlusaltdroneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blusaltdrone/BlusaltdroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blusaltdrone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlusaltdroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
